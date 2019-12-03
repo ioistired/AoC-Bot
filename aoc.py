2eff9ab85e9cbbaff8c641224eee62dda364043d
Io <io@mintz.cc>
2019-12-03 22:15:25 +0000

    documentation updates

--- aoc.py
+++ aoc.py
@@ -1,4 +1,17 @@
-#!/usr/bin/env python3
+# Copyright © 2018–2019 Io Mintz <io@mintz.cc>
+#
+# AoC Bot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# AoC Bot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with AoC Bot.  If not, see <https://www.gnu.org/licenses/>.
 
 import asyncio
 import collections
--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@
 # by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# AoC Bot is distributed in the hope that it will be fun,
+# AoC Bot is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
